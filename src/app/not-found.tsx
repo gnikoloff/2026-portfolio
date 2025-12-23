3885ecc0029825485f44cdc3b75b8946563bceca
gnikoloff <nikoloffgeorgi@gmail.com>
2025-12-23 10:35:17 +0100

    add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import PageLayout from "@/components/PageLayout";
+
+import Link from "next/link";
+import styles from "./not-found.module.css";
+
+function NotFound() {
+	return (
+		<PageLayout>
+			<div className={styles.root}>
+				<h1 className={styles.title}>404</h1>
+				<p>
+					Page not found. Go to <Link href="/">homepage</Link>.
+				</p>
+			</div>
+		</PageLayout>
+	);
+}
+
+NotFound.displayName = "NotFound";
+
+export default NotFound;
